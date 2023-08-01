a924b5765ff6d392e81e46af66f702df0481ab18
buzaaida <buzaaida95@gmail.com>
2023-08-01 16:35:28 +0200

    minor changes

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,23 +1,5 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 const admin = require('firebase-admin');
 admin.initializeApp();
 
@@ -47,7 +29,7 @@ exports.getAll = onRequest(async (req, res) => {
 exports.getByName = onRequest(async (req, res) => {
     const { name } = req.body;
     let data = [];
-    const collRef = await admin.firestore().collection('locations').where('name', '', name).get();
+    const collRef = await admin.firestore().collection('locations').where('name', '==', name).get();
     collRef.forEach(doc => {
         data.push(doc.data())
     });
@@ -56,7 +38,7 @@ exports.getByName = onRequest(async (req, res) => {
 
 exports.deleteByName = onRequest(async (req, res) => {
     const { name } = req.body;
-    const collRef = await admin.firestore().collection('locations').where('name', '', name).get();
+    const collRef = await admin.firestore().collection('locations').where('name', '==', name).get();
     collRef.forEach(doc => {
         doc.ref.delete();
     });
